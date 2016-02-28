148b1d49458745ed738de356a47d415a4edc31ad
annadeu3 <annadeu3@gmail.com>
2016-02-27 23:29:24 -0500

    added controller for todos and styled it with buttons and tables

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import todosCtrl from 'todos/todos';
 
 const app = angular.module('app', [uiRouter]);
 
@@ -9,7 +10,8 @@ app.config(($stateProvider, $urlRouterProvider,$locationProvider) => {
 	$stateProvider
 	.state('todos', {
 		url: '/',
-		template: require('todos/todos.html')
+		template: require('todos/todos.html'),
+		controller: todosCtrl
 	})
 	.state('about', {
 		url: '/about',
