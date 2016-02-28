47d89efc416bc9f2d1d465b6d693c183c7c12ed8
annadeu3 <annadeu3@gmail.com>
2016-02-28 00:47:22 -0500

    added functionality to edit, save, and cancel buttons

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -6,11 +6,13 @@ export default function ($scope) {
 	$scope.todos = [
 		{
 			task: 'do dishes',
-			isCompleted: false
+			isCompleted: false,
+			isEditting: false
 		},
 		{
 			task: 'walk the dog',
-			isCompleted: true
+			isCompleted: true,
+			isEditting: false
 		}
 	];
 
@@ -18,10 +20,27 @@ export default function ($scope) {
 		return todo.isCompleted = !todo.isCompleted;
 	};
 
+	$scope.onEditClick = function (todo) {
+		return todo.isEditting = true;
+	};
+
+	$scope.onCancelClick = todo => {
+		return todo.isEditting = false;
+	};
+
 	$scope.createTask = function () {
 		params.createHasInput = false;
 		$scope.createTaskInput = '';
 	};
+
+	$scope.updateTask = todo => {
+		todo.task = todo.updatedTask;
+		todo.isEditting = false;
+	};
+
+	// $scope.onDeleteClick = todo => {
+
+	// };
 
 	$scope.$watch('createTaskInput', val => {
 		if (!val && params.createHasInput) {
